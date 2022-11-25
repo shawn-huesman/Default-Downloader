c97ac5ca8be560217a2e830023c2205b7f2ac64d
Shawn Huesman <50417810+shawn-huesman@users.noreply.github.com>
2022-11-25 12:42:27 -0500

    Added python logging functionality for stdout and files saved locally using Docker mounted volume

--- Core/core_config.py
+++ Core/core_config.py
@@ -5,6 +5,10 @@ def get_linklist_path():
     return _get_file_location("linklist")
 
 
+def get_log_path():
+    return _get_file_location("logs")
+
+
 def _get_file_location(filename):
     with open("./User/config.json", 'r') as file:
         config = json.load(file)
--- Core/core_helper.py
+++ Core/core_helper.py
@@ -1,5 +1,10 @@
 import youtube_dl.extractor
 import logging
+import datetime
+import os
+from sys import stdout
+
+from Core.core_config import get_log_path
 
 
 def get_youtubedl_type(url):
@@ -11,3 +16,43 @@ def get_youtubedl_type(url):
             extractor_name = str(extractors).split('.')[-1].split(' ')[0]
 
     return extractor_name
+
+
+def get_logger():
+    log_format = "%(asctime)s [%(process)s] [%(threadName)-12.12s] [%(levelname)-5.5s] " \
+                 "[%(pathname)s] [%(funcName)s:%(lineno)d] \n %(message)s \n"
+    log_formatter = logging.Formatter(log_format)
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+
+    log_path = get_log_path()
+    file_name = timestamp_filename("ddlog")
+    file_handler_path = os.path.join(log_path, timestamp_ymd())
+    make_dirs_if_not_exist([file_handler_path])
+    file_handler_filepath = os.path.join(file_handler_path, file_name + ".log")
+
+    file_handler = logging.FileHandler(file_handler_filepath)
+    file_handler.setFormatter(log_formatter)
+    root_logger.addHandler(file_handler)
+
+    consoleHandler = logging.StreamHandler(stdout)
+    consoleHandler.setFormatter(log_formatter)
+    root_logger.addHandler(consoleHandler)
+
+    return root_logger
+
+
+def timestamp_filename(filename):
+    format = "%Y-%m-%d %H.%M.%S {fname}".format(fname=filename)
+    return datetime.datetime.now().strftime(format)
+
+
+def timestamp_ymd():
+    return datetime.datetime.now().strftime("%Y-%m-%d")
+
+
+def make_dirs_if_not_exist(list_of_dirs):
+    for directory in list_of_dirs:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
