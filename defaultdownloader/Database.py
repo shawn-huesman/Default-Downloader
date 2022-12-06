91c8d7cee415c56c95dc187361463b328aac8410
Shawn Huesman <50417810+shawn-huesman@users.noreply.github.com>
2022-12-05 23:34:37 -0500

    Refactored Database get, insert, remove, and update functions and added youtube playlist functions

--- defaultdownloader/Database.py
+++ defaultdownloader/Database.py
@@ -6,6 +6,7 @@ class DefaultDB(object):
     DATABASE = None
     DEFAULTDOWNLOADERDB = "defaultdownloaderdb"
     YOUTUBEVIDEODB = "youtubevideodb"
+    YOUTUBEPLAYLISTDB = "youtubeplaylistdb"
 
     @staticmethod
     def initialize(database_name):
@@ -24,44 +25,83 @@ class DefaultDB(object):
         return docs
 
     @staticmethod
-    def get_linklist_docs():
-        return DefaultDB.get_docs_by_date_added(DefaultDB.DEFAULTDOWNLOADERDB, "linklist")
+    def get_docs_to_download(db, collection, url_type, only_docs_not_downloaded=True):
+        collection = DefaultDB.get_collection(db, collection)
+
+        docs = []
+        for doc in collection.find({}):
+            if doc["url_type"] == url_type:
+                if (only_docs_not_downloaded and not doc["downloaded"]) or not only_docs_not_downloaded:
+                    docs.append(doc)
+        return docs
 
     @staticmethod
-    def linklist_insert(data):
-        collection = DefaultDB.get_collection(DefaultDB.DEFAULTDOWNLOADERDB, 'linklist')
-
-        if collection.count_documents({"url": data["url"]}) == 0:
-            collection.insert_one(data)
-            return True
-        return False
-
-    @staticmethod
-    def linklist_remove(data, match_field="url"):
-        collection = DefaultDB.get_collection(DefaultDB.DEFAULTDOWNLOADERDB, 'linklist')
+    def remove(db, collection, data, match_field="url"):
+        collection = DefaultDB.get_collection(db, collection)
         matching_docs = collection.find({match_field: data[match_field]})
 
         if matching_docs:
             for doc in matching_docs:
                 collection.delete_one(doc)
+                return True
         return False
 
     @staticmethod
-    def youtube_insert(data):
-        collection = DefaultDB.get_collection(DefaultDB.YOUTUBEVIDEODB, 'videos')
+    def insert(db, collection, data, match_field="url"):
+        collection = DefaultDB.get_collection(db, collection)
 
-        if collection.count_documents({"url": data["url"]}) == 0:
+        if collection.count_documents({match_field: data[match_field]}) == 0:
             collection.insert_one(data)
             return True
         return False
 
     @staticmethod
-    def youtube_get_videos():
-        collection = DefaultDB.get_collection(DefaultDB.YOUTUBEVIDEODB, 'videos')
+    def update_as_downloaded(db, collection, doc, upsert=False):
+        collection = DefaultDB.get_collection(db, collection)
 
-        youtube_videos = []
-        for doc in collection.find({}):
-            if doc["url_type"] == "youtube-video" and not doc['downloaded']:
-                youtube_videos.append(doc)
+        if collection.update_one({'_id': doc['_id']}, {'$set': {'downloaded': True}}, upsert=upsert):
+            return True
+        return False
 
-        return youtube_videos
+    @staticmethod
+    def get_linklist_docs():
+        return DefaultDB.get_docs_by_date_added(DefaultDB.DEFAULTDOWNLOADERDB, "linklist")
+
+    @staticmethod
+    def linklist_insert(data):
+        return DefaultDB.insert(DefaultDB.DEFAULTDOWNLOADERDB, "linklist", data)
+
+    @staticmethod
+    def linklist_remove(data):
+        return DefaultDB.remove(DefaultDB.DEFAULTDOWNLOADERDB, "linklist", data)
+
+    @staticmethod
+    def youtube_videos_insert(data):
+        return DefaultDB.insert(DefaultDB.YOUTUBEVIDEODB, "videos", data)
+
+    @staticmethod
+    def youtube_get_videos_to_download():
+        return DefaultDB.get_docs_to_download(DefaultDB.YOUTUBEVIDEODB, "videos", "youtube-video")
+
+    @staticmethod
+    def youtube_update_video_as_downloaded(doc):
+        return DefaultDB.update_as_downloaded(DefaultDB.YOUTUBEVIDEODB, "videos", doc)
+
+    @staticmethod
+    def youtube_playlist_insert(data, playlist_id):
+        return DefaultDB.insert(DefaultDB.YOUTUBEPLAYLISTDB, playlist_id, data)
+
+    @staticmethod
+    def youtube_get_playlist_videos_to_download():
+        client = MongoClient(DefaultDB.URI)
+        DefaultDB.DATABASE = client[DefaultDB.YOUTUBEPLAYLISTDB]
+
+        docs = []
+        for playlist_collection in DefaultDB.DATABASE.list_collection_names():
+            docs = docs + DefaultDB.get_docs_to_download(DefaultDB.YOUTUBEPLAYLISTDB, playlist_collection,
+                                                         "youtube-playlist-video")
+        return docs
+
+    @staticmethod
+    def youtube_update_playlist_video_as_downloaded(doc, playlist_id):
+        return DefaultDB.update_as_downloaded(DefaultDB.YOUTUBEPLAYLISTDB, playlist_id, doc)
