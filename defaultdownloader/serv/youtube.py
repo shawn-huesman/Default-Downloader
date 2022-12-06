d7ef07d25fe43231e0347b0afd31be62f0957119
Shawn Huesman <50417810+shawn-huesman@users.noreply.github.com>
2022-12-05 23:44:18 -0500

    Refactored youtube pipeline by removing clean and update function and adding youtube playlist populate

--- defaultdownloader/serv/youtube.py
+++ defaultdownloader/serv/youtube.py
@@ -9,13 +9,15 @@ from defaultdownloader.core.download import download_video_using_ytdlp
 
 logger = get_logger(__name__)
 
+supported_populate_types = ["youtube-video", "youtube-playlist", "youtube-channel"]
+supported_load_types = ["youtube-video", "youtube-playlist-video", "youtube-channel-video"]
+
 
 def run():
     logger.info("Running YouTube pipeline...")
 
     load()
     input_dict = read()
-    update(input_dict)
     download(input_dict)
     organize()
 
@@ -27,33 +29,51 @@ def load():
         data = populate(doc)
 
         for data_entry in data:
-            inserted = False
-            if DefaultDB.youtube_insert(data_entry):
-                inserted = True
+            url_type = data_entry["url_type"]
+            if url_type in supported_load_types:
+                if url_type == "youtube-video":
+                    DefaultDB.youtube_videos_insert(data_entry)
 
-            if inserted:
+                elif url_type == "youtube-playlist-video":
+                    playlist_id = data_entry["playlist_id"]
+                    DefaultDB.youtube_playlist_insert(data_entry, playlist_id)
+
                 DefaultDB.linklist_remove(data_entry)
+
+
+def populate(doc) -> list:
+    data = {}
+
+    if "url_type" in doc:
+        if doc["url_type"] in supported_populate_types:
+            url = doc["url"]
+            url_type = doc["url_type"]
+
+            if url_type == "youtube-video":
+                data = _get_video_data(url)
+
+            elif url_type == "youtube-playlist":
+                data = _get_playlist_data(url)
+
+    if isinstance(data, list):
+        return data
+    else:
+        return [data]
 
 
 def read():
     input_dict = {}
-    input_dict.update({'youtube_videos': DefaultDB.youtube_get_videos()})
+    input_dict.update({'youtube_videos': DefaultDB.youtube_get_videos_to_download()})
+    input_dict.update({'youtube_playlist_videos': DefaultDB.youtube_get_playlist_videos_to_download()})
 
     return input_dict
-
-
-def clean():
-    pass
-
-
-def update(input_dict):
-    pass
 
 
 def download(input_dict):
     youtube_path = _get_youtube_data_path()
     youtube_video_path = os.path.join(youtube_path, "Videos")
-    make_dirs_if_not_exist([youtube_path, youtube_video_path])
+    youtube_playlist_path = os.path.join(youtube_path, "Playlists")
+    make_dirs_if_not_exist([youtube_path, youtube_video_path, youtube_playlist_path])
 
     for doc in input_dict['youtube_videos']:
         url = doc["url"]
@@ -77,22 +97,44 @@ def organize():
     pass
 
 
-def populate(doc) -> list:
-    supported_types = ["youtube-video", "youtube-playlist", "youtube-channel"]
-    data = {}
+def _get_playlist_data(url):
+    YDL_OPTIONS = {
+        'ignoreerrors': True
+    }
 
-    if "url_type" in doc:
-        if doc["url_type"] in supported_types:
-            url = doc["url"]
-            url_type = doc["url_type"]
+    url_type = "youtube-playlist-video"
+    data = []
+    with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+        info = ydl.extract_info(url, download=False)
 
-            if url_type == "youtube-video":
-                data = _get_video_data(url)
+        if "entries" in info:
+            video_list = info["entries"]
 
-    if isinstance(data, list):
-        return data
-    else:
-        return [data]
+            for i, item in enumerate(video_list):
+                video = info["entries"][i]
+
+                playlist_video_url = video.get('webpage_url', None)
+                title = video.get('title', None)
+                uploader = video.get('uploader', None)
+                playlist_name = video.get('playlist', None)
+                playlist_index = video.get('playlist_index', None)
+                playlist_id = video.get('playlist_id', None)
+
+                video_data = {
+                    "url": playlist_video_url,
+                    "title": title,
+                    "uploader": uploader,
+                    "downloaded": False,
+                    "date_added": get_current_date(),
+                    "url_type": url_type,
+                    "playlist_name": playlist_name,
+                    "playlist_index": playlist_index,
+                    "playlist_id": playlist_id
+                }
+
+                data.append(video_data)
+
+    return data
 
 
 def _get_video_data(url):
@@ -102,9 +144,9 @@ def _get_video_data(url):
 
     url_type = "youtube-video"
     with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
-        info_dict = ydl.extract_info(url, download=False)
-        title = info_dict.get('title', None)
-        uploader = info_dict.get('uploader', None)
+        info = ydl.extract_info(url, download=False)
+        title = info.get('title', None)
+        uploader = info.get('uploader', None)
 
         data = {
             "url": url,
