bd9cb886fb1763e48831b4ac97db7ff4bf6a4645
Shawn Huesman <50417810+shawn-huesman@users.noreply.github.com>
2022-12-09 01:20:36 -0500

    Fixed ytdlp extractor not grabbing any type

--- defaultdownloader/core/helper.py
+++ defaultdownloader/core/helper.py
@@ -12,8 +12,8 @@ def get_ytdlp_type(url):
 
     extractor_name = None
     for extractor in extractors:
-        if extractor.suitable(url) and extractors.IE_NAME != 'generic':
-            extractor_name = str(extractors).split('.')[-1].split(' ')[0]
+        if extractor.suitable(url) and extractor.IE_NAME != 'generic':
+            extractor_name = str(extractor).split('.')[-1].split(' ')[0]
 
     return extractor_name
 
